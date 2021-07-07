18c356a99a541c2827b3b0db2d218e0a38cefce3
Masahiro Endo <58802232+masahiro-endo@users.noreply.github.com>
2021-07-07 14:39:38 +0900

    test

---
+++ pyg/020-basis/basic/myfont/myfont.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import pygame
+from pygame.locals import *
+import sys
+
+SCREEN_SIZE = (640, 480)
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("フォントファイルのロード")
+
+# フォントの作成
+myfont = pygame.font.Font("ipag.ttf", 80)
+
+# テキストを描画したSurfaceを作成
+hello1 = myfont.render(u"こんにちは！", False, (0,0,0))
+hello2 = myfont.render(u"こんにちは！", True, (0,0,0))
+hello3 = myfont.render(u"こんにちは！", True, (255,0,0), (255,255,0))
+
+while True:
+    screen.fill((0,0,255))
+
+    # テキストを描画する
+    screen.blit(hello1, (20,50))
+    screen.blit(hello2, (20,150))
+    screen.blit(hello3, (20,250))
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
