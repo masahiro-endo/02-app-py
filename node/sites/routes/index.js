596f834af159e9c07141a647ae74fa05f956105b
Masahiro Endo <58802232+masahiro-endo@users.noreply.github.com>
2021-11-19 11:57:32 +0900

    1

---
+++ node/sites/routes/index.js
@@ -0,0 +1,22 @@
+
+const express = require('express')
+
+var app = express.Router();
+
+
+// indexのテンプレートを呼び出す
+app.get('/', function(req, res) {
+    var mascots = [
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+      { name: 'Tux', organization: "Linux", birth_year: 1996},
+      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+  
+    res.render('pages/index', {
+      mascots: mascots,
+      tagline: tagline
+    });
+});
+
+
