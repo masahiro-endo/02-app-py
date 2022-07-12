678a6bbdc2efaf7229db7be125ff11c3d358f44e
Masahiro Endo <58802232+masahiro-endo@users.noreply.github.com>
2022-07-12 16:50:12 +0900

    1

--- node/express/sample/app.js
+++ node/express/sample/app.js
@@ -3,6 +3,10 @@
  var fs = require('fs');
 
  const express = require('express');
+ var session = require("express-session");
+ var bodyParser = require("body-parser");
+ var cookie = require("cookie-parser");
+ 
  const app = express();
  
  const port = 8080
@@ -10,7 +14,26 @@
 
  
 
- app.use(express.static(path.join(__dirname, 'public')));
+ // ビューエンジンをejsにセットする
+app.set('views', path.join(__dirname, 'views/'));
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').renderFile);
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookie());
+//express-sessionモジュールを設定する
+app.use(session( {
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1 * 24 * 60 * 1000,
+      secure: false
+    }
+}));
+
+
 
  // ルーティング設定
  var files = fs.readdirSync(__dirname + '/routes');
